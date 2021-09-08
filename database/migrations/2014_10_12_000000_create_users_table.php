9d2b9d5548ec2fa121e5110e32d20c5d9a6f1e3f
Coderkhayrul <coderkhayrul>
2021-09-08 16:28:06 +0600

    Doctor Appointent

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('role_id')->default('1');
+            $table->string('address')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('department')->nullable();
+            $table->string('image')->nullable();
+            $table->string('education')->nullable();
+            $table->text('description')->nullable();
+            $table->text('gender')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,5 @@
+@include('admin.layouts.header')
+@include('admin.layouts.sidebar')
+@yield('content')
+@include('admin.layouts.footer')
+
